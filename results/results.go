6ceab10d4e8a23551ff0c2cdd7fb5a9f93a95fbc
David Kowis <dkowis@shlrm.org>
2013-08-17 20:50:19 -0500

    adding the results from my machine, for posterity

---
+++ results/results.go
@@ -0,0 +1,43 @@
+This is ApacheBench, Version 2.3 <$Revision: 1430300 $>
+Copyright 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/
+Licensed to The Apache Software Foundation, http://www.apache.org/
+
+Benchmarking localhost (be patient)
+
+
+Server Software:        
+Server Hostname:        localhost
+Server Port:            8000
+
+Document Path:          /
+Document Length:        1048576 bytes
+
+Concurrency Level:      100
+Time taken for tests:   4.357 seconds
+Complete requests:      10000
+Failed requests:        0
+Write errors:           0
+Total transferred:      10486920000 bytes
+HTML transferred:       10485760000 bytes
+Requests per second:    2295.42 [#/sec] (mean)
+Time per request:       43.565 [ms] (mean)
+Time per request:       0.436 [ms] (mean, across all concurrent requests)
+Transfer rate:          2350768.06 [Kbytes/sec] received
+
+Connection Times (ms)
+              min  mean[+/-sd] median   max
+Connect:        0    1   0.2      1       3
+Processing:     2   43   3.0     43      62
+Waiting:        0    1   1.5      1      25
+Total:          5   43   2.9     44      63
+
+Percentage of the requests served within a certain time (ms)
+  50%     44
+  66%     44
+  75%     45
+  80%     45
+  90%     46
+  95%     47
+  98%     47
+  99%     52
+ 100%     63 (longest request)
---
+++ results/results.scala
@@ -0,0 +1,43 @@
+This is ApacheBench, Version 2.3 <$Revision: 1430300 $>
+Copyright 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/
+Licensed to The Apache Software Foundation, http://www.apache.org/
+
+Benchmarking localhost (be patient)
+
+
+Server Software:        spray-can/1.1-M8
+Server Hostname:        localhost
+Server Port:            8000
+
+Document Path:          /
+Document Length:        1048576 bytes
+
+Concurrency Level:      100
+Time taken for tests:   10.698 seconds
+Complete requests:      10000
+Failed requests:        0
+Write errors:           0
+Total transferred:      10487230000 bytes
+HTML transferred:       10485760000 bytes
+Requests per second:    934.80 [#/sec] (mean)
+Time per request:       106.975 [ms] (mean)
+Time per request:       1.070 [ms] (mean, across all concurrent requests)
+Transfer rate:          957364.87 [Kbytes/sec] received
+
+Connection Times (ms)
+              min  mean[+/-sd] median   max
+Connect:        0    2   1.1      1       6
+Processing:    10  105  11.3    104     215
+Waiting:        2   10   9.5      7     110
+Total:         13  107  11.2    106     217
+
+Percentage of the requests served within a certain time (ms)
+  50%    106
+  66%    108
+  75%    110
+  80%    112
+  90%    117
+  95%    123
+  98%    134
+  99%    142
+ 100%    217 (longest request)
